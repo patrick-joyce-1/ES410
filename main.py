e5122e6666f472e1e6a88370d002e7eb7c4540c6
patrick-joyce-1 <78419430+patrick-joyce-1@users.noreply.github.com>
2022-02-16 15:31:08 +0000

    Update main.py

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ def main():
                         motorposition -= 1.8
                     elif curr_value_pin_b == GPIO.HIGH:  # Clockwise
                         motorposition += 1.8
-                    time.sleep(0.25)
+                    time.sleep(1)
                     print("Motor position is: ", motorposition)
 
     except KeyboardInterrupt:
