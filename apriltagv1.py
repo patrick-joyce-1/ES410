78b827b9a854909705e67db0d0b9d21bda50931c
patrick-joyce-1 <78419430+patrick-joyce-1@users.noreply.github.com>
2022-02-22 23:19:21 +0000

    Update apriltagv1.py

--- apriltagv1.py
+++ apriltagv1.py
@@ -1,6 +1,6 @@
 import apriltag
 import cv2
-path = r'/Home/lib-dt-apriltags/dt_apriltags/photo.jpg'
+path = r'/Home/lib-dt-apriltags/dt_apriltags/photo.png'
 img = cv2.imread(path,0)
 detector = apriltag.Detector()
 result = detector.detect(img)
