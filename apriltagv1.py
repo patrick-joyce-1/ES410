aec1bdf389ea190e2c5c1a11614cdbf0b8b15c4a
patrick-joyce-1 <78419430+patrick-joyce-1@users.noreply.github.com>
2022-02-23 14:56:39 +0000

    Update apriltagv1.py

--- apriltagv1.py
+++ apriltagv1.py
@@ -1,8 +1,12 @@
 import apriltag
 import cv2
-path = r'/Home/lib-dt-apriltags/dt_apriltags/photo.png'
-img = cv2.imread(path,0)
+from PIL import Image
+from numpy import asarray
+img = Image.open('photo.jpeg')
+imggrey = img.convert('L')
+data = asarray(imggrey)
 detector = apriltag.Detector()
-result = detector.detect(img)
+result = detector.detect(data)
 
 print(result)
+print(result[0][6][0])
