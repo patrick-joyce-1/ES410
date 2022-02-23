eb4a5806628f0abbc4021c89546513eb3547270e
patrick-joyce-1 <78419430+patrick-joyce-1@users.noreply.github.com>
2022-02-23 15:56:10 +0000

    Update photov2.py

--- photov2.py
+++ photov2.py
@@ -1,8 +1,35 @@
-import cv2  # import the opencv library
+import cv2  # import the opencv libraryr
+def gstreamer_pipeline(
+    capture_width=1920,
+    capture_height=1080,
+    display_width=1920,
+    display_height=1080,
+    framerate=30,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, "
+        "format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+
 filename = 'savedImage.jpg'
 
 # define a video capture object
-vid = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+vid = cv2.VideoCapture(gstreamer_pipeline(flip_method=2), cv2.CAP_GSTREAMER)
 
 # Capture the video frame by frame
 ret, frame = vid.read()
@@ -10,4 +37,3 @@ ret, frame = vid.read()
 cv2.imwrite(filename, frame)  # Using cv2.imwrite() method to the image
 
 vid.release()  # After the loop release the cap object
-
